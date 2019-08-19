091813f0028dcd1e7b843b059b4db48c49e2f376
achan <abdurhasan20@gmail.com>
2019-08-19 13:25:02 +0700

    crot

---
+++ server.js
@@ -0,0 +1,44 @@
+const redis = require('redis');
+const client = redis.createClient();
+const port = process.env.PORT || 3000;
+const fastify = require('fastify')({
+    logger: true
+})
+
+
+fastify.get('/', (req, res) => {
+    const { image, keystore, expire } = req.query;
+
+    if (!image || !keystore) {
+        return res.callNotFound()
+    }
+    const expired = expire ? expire : 600;
+    client.setex(keystore, expired, image);
+    res.send({ success: true, url_image: `http://${req.headers.host}/image/${keystore}` })
+
+})
+
+fastify.get('/image/:keystore', (req, res) => {
+    const keystore = req.params.keystore;
+    client.get(keystore, function (err, data) {
+        if (err) throw err;
+
+        if (data != null) {
+            const base64Data = data.replace(/^data:image\/png;base64,/, "").replace(/ /g, "+");
+            const img = Buffer.from(base64Data, 'base64');
+            res.header('Content-Type', 'image/png')
+            res.send(img)
+
+        } else {
+            res.callNotFound()
+        }
+    });
+
+})
+
+
+// Run the server!
+fastify.listen(port, (err, address) => {
+    if (err) throw err
+    fastify.log.info(`server listening on ${address}`)
+})
